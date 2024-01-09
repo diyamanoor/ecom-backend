21340fb6e62c3fbec04e7906e32bb066a434c249
diyamanoor <divyakmanoor@gmail.com>
2024-01-09 13:13:40 +0530

    Created the products list file

---
+++ products.js
@@ -0,0 +1,40 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+      "https://res.cloudinary.com/ddpnhkmsw/image/upload/v1704699316/fmg9evvt8zvxpxclq8we.jpg",
+  },
+  {
+    id: 2,
+    name: "OnePlus 10",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image:
+      "https://res.cloudinary.com/ddpnhkmsw/image/upload/v1704699759/yltkjfqjgpjy1iyoxc7r.jpg",
+  },
+  {
+    id: 3,
+    name: "Samsung Galaxy S22",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/ddpnhkmsw/image/upload/v1704699471/tta5tsc98dauiumgdre0.jpg",
+  },
+  {
+    id: 4,
+    name: "iPhone 15 Pro",
+    brand: "Apple",
+    desc: "7 -inch display",
+    price: 2500,
+    image:
+      "https://res.cloudinary.com/ddpnhkmsw/image/upload/v1704713236/frmurqioatgbzl9mnijc.jpg",
+  },
+];
+
+module.exports = products;
